bb38fc94b7325410a2f6964bb824f5bc1dc48b36
Francisco Sousa <sousa.fco@hotmail.com>
2023-06-16 22:47:41 -0300

    comunicação com api

--- acesso/views.py
+++ acesso/views.py
@@ -1,7 +1,10 @@
+import requests
 from django.contrib import messages
 from django.shortcuts import render
 
 from acesso.forms import LoginForm
+from core.settings import URL_API
+
 
 # Create your views here.
 
@@ -10,7 +13,18 @@ def login(request):
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
-            messages.success(request, 'modificado com sucesso')
+            auth_data = {
+                'login': form.cleaned_data.get('username'),
+                'senha': form.cleaned_data.get('password')
+            }
+            response = requests.post(URL_API, json=auth_data)
+            # messages.success(request, response.status_code)
+            if response.status_code == 200:
+                messages.success(request, response.text)
+                messages.success(request, response.json())
+            else:
+                messages.success(request, 'erro')
+
     else:
         form = LoginForm()
     return render(request, template_name, {"form": form})
