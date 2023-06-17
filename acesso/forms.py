51f7f0c099d852a83475231cc978d27c316eb548
Francisco Sousa <sousa.fco@hotmail.com>
2023-06-17 15:13:36 -0300

    autofocus

--- acesso/forms.py
+++ acesso/forms.py
@@ -3,3 +3,7 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(label="Usuário", max_length=100)
     password = forms.CharField(label="Senha", widget=forms.PasswordInput())
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'autofocus': ''})
