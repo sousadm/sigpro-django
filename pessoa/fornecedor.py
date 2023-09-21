fadc3a0bf946a8c04acbd1d923a9174f1a666a0b
Francisco Sousa <sousa.fco@hotmail.com>
2023-09-21 10:47:36 -0300

    (alt) cadastro de pessoa revisado

---
+++ pessoa/fornecedor.py
@@ -0,0 +1,67 @@
+import json
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django import forms
+from core.controle import require_token
+
+from core.tipos import TIPO_SITUACAO
+from pessoa.forms import ativar_pessoa_tipo, existe_registro, pesquisa_pessoa, salvar_pessoa_tipo
+
+
+@require_token
+def pessoaFornecedorEdit(request, uuid):
+    template_name = 'pessoa/fornecedor_edit.html'
+    form = FornecedorForm()
+    try:
+
+        if request.POST.get('btn_pessoa'):
+            return HttpResponseRedirect(reverse('url_pessoa_edit', kwargs={'uuid': uuid}))
+
+        if request.POST.get('btn_salvar'):
+            form = FornecedorForm(request.POST)
+            form.salvar(request)
+            messages.success(request, 'sucesso ao gravar dados')
+            return HttpResponseRedirect(reverse('url_pessoa_fornecedor', kwargs={'uuid': uuid}))
+
+        if request.POST.get('btn_ativar'):
+            form.ativar(request, request.POST.get('fornecedorId'))
+            return HttpResponseRedirect(reverse('url_pessoa_fornecedor', kwargs={'uuid': uuid}))
+
+    except Exception as e:
+        messages.error(request, e)
+
+    form.pesquisaPorPessoa(request, uuid)
+    return render(request, template_name, {'form': form})
+
+
+class FornecedorForm(forms.Form):
+    pessoaId = forms.IntegerField()
+    nome = forms.CharField(max_length=100, label='Nome', disabled=True)    
+    fornecedorId = forms.IntegerField(label='Código', required=False, disabled=True)
+    situacaoFornecedor = forms.ChoiceField(choices=TIPO_SITUACAO, initial=True)
+    # nivelNegociacao = forms.ChoiceField(choices=NIVEL_NEGOCIACAO, initial='SEM_DESCONTO')    
+    created_dt = forms.DateTimeField(label='Data do cadastro', required=False)
+
+    def existe(self):
+        return existe_registro(self, self.initial, 'fornecedorId')
+
+    def pesquisaPorPessoa(self, request, uuid):
+        self.initial = pesquisa_pessoa(self, request, uuid, 'fornecedor')
+
+    def json(self):
+        post_data = dict(self.data)  # Converter QueryDict para dicionário
+        post_data.pop('csrfmiddlewaretoken', None)
+        post_data.pop('btn_salvar', None)
+        json_data = json.dumps(post_data).replace("[", "").replace("]", "")
+        data = json.loads(json_data)
+        if data['fornecedorId'] == 'None': data['fornecedorId'] = None
+        return data
+
+    def ativar(self, request, uuid):
+        ativar_pessoa_tipo(self, request, 'fornecedor', uuid)
+
+    def salvar(self, request):
+        data = self.json()
+        salvar_pessoa_tipo(self, request, data, 'fornecedor')
--- pessoa/transportador.py
+++ pessoa/transportador.py
@@ -6,25 +6,25 @@ from django.urls import reverse
 from django import forms
 from core.controle import require_token
 
-from core.tipos import NIVEL_NEGOCIACAO, TIPO_SITUACAO
+from core.tipos import TIPO_PROPRIETARIO, TIPO_SITUACAO
 from pessoa.forms import ativar_pessoa_tipo, existe_registro, pesquisa_pessoa, salvar_pessoa_tipo
 
 
-# VENDEDOR
-class VendedorForm(forms.Form):
+# TRANSPORTADOR
+class TransportadorForm(forms.Form):
     pessoaId = forms.IntegerField()
     nome = forms.CharField(max_length=100, label='Nome', disabled=True)
-    vendedorId = forms.IntegerField(label='Código', required=False, disabled=True)
-    situacaoVendedor = forms.ChoiceField(choices=TIPO_SITUACAO, initial=True)
-    nivelNegociacao = forms.ChoiceField(choices=NIVEL_NEGOCIACAO, initial=True)
-    comissao = forms.FloatField(label='Percentual de Comissão', initial=0, widget=forms.DateInput(attrs={'autofocus': 'true', }))
+    transportadorId = forms.IntegerField(label='Código', required=False, disabled=True)
+    situacaoTransportador = forms.ChoiceField(choices=TIPO_SITUACAO, initial=True)
+    codigoRNTRC = forms.CharField(max_length=20, label='RNTRC', required=False) 
+    tipoProprietario = forms.ChoiceField(choices=TIPO_PROPRIETARIO, initial="OUTROS", label="Tipo de Proprietário")
     created_dt = forms.DateTimeField(label='Data do cadastro', required=False)
 
     def existe(self):
-        return existe_registro(self, self.initial, 'vendedorId')
+        return existe_registro(self, self.initial, 'transportadorId')
 
     def pesquisaPorPessoa(self, request, uuid):
-        self.initial = pesquisa_pessoa(self, request, uuid, 'vendedor')
+        self.initial = pesquisa_pessoa(self, request, uuid, 'transportador')
 
     def json(self):
         post_data = dict(self.data)  # Converter QueryDict para dicionário
@@ -32,35 +32,35 @@ class VendedorForm(forms.Form):
         post_data.pop('btn_salvar', None)
         json_data = json.dumps(post_data).replace("[", "").replace("]", "")
         data = json.loads(json_data)
-        if data['vendedorId'] == 'None': data['vendedorId'] = None
+        if data['transportadorId'] == 'None': data['transportadorId'] = None
         return data
 
     def ativar(self, request, uuid):
-        ativar_pessoa_tipo(self, request, 'vendedor', uuid)
+        ativar_pessoa_tipo(self, request, 'transportador', uuid)
 
     def salvar(self, request):
         data = self.json()
-        salvar_pessoa_tipo(self, request, data, 'vendedor')
+        salvar_pessoa_tipo(self, request, data, 'transportador')
 
 
 @require_token
-def pessoaVendedorEdit(request, uuid):
-    template_name = 'pessoa/vendedor_edit.html'
-    form = VendedorForm()
+def pessoaTransportadorEdit(request, uuid):
+    template_name = 'pessoa/transportador_edit.html'
+    form = TransportadorForm()
     try:
 
         if request.POST.get('btn_pessoa'):
             return HttpResponseRedirect(reverse('url_pessoa_edit', kwargs={'uuid': uuid}))
 
         if request.POST.get('btn_salvar'):
-            form = VendedorForm(request.POST)
+            form = TransportadorForm(request.POST)
             form.salvar(request)
             messages.success(request, 'sucesso ao gravar dados')
-            return HttpResponseRedirect(reverse('url_pessoa_vendedor', kwargs={'uuid': uuid}))
+            return HttpResponseRedirect(reverse('url_pessoa_transportador', kwargs={'uuid': uuid}))
 
         if request.POST.get('btn_ativar'):
-            form.ativar(request, request.POST.get('vendedorId'))
-            return HttpResponseRedirect(reverse('url_pessoa_vendedor', kwargs={'uuid': uuid}))
+            form.ativar(request, request.POST.get('transportadorId'))
+            return HttpResponseRedirect(reverse('url_pessoa_transportador', kwargs={'uuid': uuid}))
 
     except Exception as e:
         messages.error(request, e)
